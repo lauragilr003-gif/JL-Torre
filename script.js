2019d9a94fb2603d2931ab7440838bee4d1097ea
lauragilr003-gif <lauragilr003@gmail.com>
2026-05-02 22:14:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,18 +9,17 @@ const audioFinal = document.getElementById('snd-final');
 
 let encendido = false;
 
-// Niveles de mezcla de post-producción
+// Mezcla de audio
 audioEstatica.volume = 0.1; 
-audioCambio.volume = 0.8;
+audioCambio.volume = 0.9;
 audioFinal.volume = 1.0;
 
 power.addEventListener('click', () => {
     encendido = !encendido;
     power.classList.toggle('on');
-    document.querySelector('.estado-texto').innerText = encendido ? "ON" : "OFF";
     
     if (encendido) {
-        audioEstatica.play();
+        audioEstatica.play().catch(e => console.log("Carga audios en GitHub"));
     } else {
         [audioEstatica, audioCambio, audioFinal].forEach(a => { a.pause(); a.currentTime = 0; });
     }
@@ -30,18 +29,17 @@ perilla.addEventListener('input', () => {
     if (!encendido) return;
     
     let val = perilla.value;
-    // Mantenemos el cálculo de frecuencia
     let mhz = (88.0 + (val * 0.2)).toFixed(1);
     mhzDisp.innerText = mhz;
     
-    // Mover aguja (la aguja se mueve horizontal aunque el fader sea vertical)
+    // Mover aguja
     aguja.style.left = (45.4 + (val * 0.33)) + "%";
 
-    // Sonido de sintonía fuerte al deslizar
+    // Sonido sintonía dominante
     audioCambio.currentTime = 0;
     audioCambio.play();
 
-    // Sintonía del caso JL Torres
+    // Sintonía exacta para el Caso JL Torres
     if (parseFloat(mhz) === 99.8) {
         audioEstatica.pause();
         audioFinal.play();
